b32525238dabbf51c31a49ab570129eea3612c54
EllaSEON <shineunhye5@gmail.com>
2023-04-18 16:01:44 +0900

    <feat> : 배경화면 클릭시 모달창 닫기

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 import { useAppDispatch } from "../../../store/hooks";
@@ -8,11 +9,19 @@ interface ModalProps {
 }
 
 function Modal({ children }: ModalProps) {
+  const backgroundRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  // 배경화면 클릭시 모달창 닫기
+  const handleClickOutside = (e: React.MouseEvent<HTMLElement>) => {
+    if (backgroundRef.current === e.target) {
+      dispatch(closeModal());
+    }
+  };
+
   return (
-    <S.Background>
+    <S.Background ref={backgroundRef} onClick={handleClickOutside}>
       <S.ModalContainer>
         <S.ModalText>{children}</S.ModalText>
         <S.ModalBtn
