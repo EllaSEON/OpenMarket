60697f6adbe2e2b8d300d57a7e5dc17862c45c05
EllaSEON <shineunhye5@gmail.com>
2023-04-24 15:24:13 +0900

    <refact> : 상품 상세 스타일 map 으로 리팩토링

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -1,45 +1,32 @@
 import { useState } from "react";
 import * as S from "./style";
 
+const TAB_ITEMS = [
+  { id: 1, title: "상품상세" },
+  { id: 2, title: "상품평" },
+  { id: 3, title: "상품문의" },
+  { id: 4, title: "배송/교환/반품 안내" },
+];
+
 function ProductDetail() {
-  const [tableContent, setTableContent] = useState("상품 상세");
+  const [activeTabId, setActiveTabId] = useState(1);
+
+  const activeTabTitle = TAB_ITEMS.find((tab) => tab.id === activeTabId)?.title;
+
   return (
     <div>
       <S.TabTitleWrapper>
-        <S.TabTitle
-          active={tableContent === "상품상세"}
-          onClick={() => {
-            setTableContent("상품상세");
-          }}
-        >
-          상품상세
-        </S.TabTitle>
-        <S.TabTitle
-          active={tableContent === "상품평"}
-          onClick={() => {
-            setTableContent("상품평");
-          }}
-        >
-          상품평
-        </S.TabTitle>
-        <S.TabTitle
-          active={tableContent === "상품문의"}
-          onClick={() => {
-            setTableContent("상품문의");
-          }}
-        >
-          상품문의
-        </S.TabTitle>
-        <S.TabTitle
-          active={tableContent === "배송/교환/반품 안내"}
-          onClick={() => {
-            setTableContent("배송/교환/반품 안내");
-          }}
-        >
-          배송/교환/반품 안내
-        </S.TabTitle>
+        {TAB_ITEMS.map((tab) => (
+          <S.TabTitle
+            key={tab.id}
+            active={tab.id === activeTabId}
+            onClick={() => setActiveTabId(tab.id)}
+          >
+            {tab.title}
+          </S.TabTitle>
+        ))}
       </S.TabTitleWrapper>
-      <S.TabContent>{tableContent}</S.TabContent>
+      <S.TabContent>{activeTabTitle}</S.TabContent>
     </div>
   );
 }
